6b9a28d86f438284fb77c3f9b193ba241d29fff4
Aditi Sharma <sharmaaditi.nitsri@gmail.com>
2016-05-08 14:08:24 +0530

    Interview Questions

--- src/Arrays/MaximumAverageSubArray.java
+++ src/Arrays/MaximumAverageSubArray.java
@@ -4,4 +4,44 @@ package Arrays;
  * Created by 61310444 on 5/7/2016.
  */
 public class MaximumAverageSubArray {
+    public static void main(String[] args) {
+        int[] input= {11, -8, 16, -7, 24, -2, 3};
+        System.out.println(maxAverageArray(input, 3));
+
+    }
+
+    public static int  maxAverageArray(int[] arr,int k)
+    {
+       if (arr.length<k)
+       {
+           System.out.println("Invalid Argument");
+           return 0;
+       }
+
+        int curr_sum=0,max_sum=0;
+         for (int i=0;i<k;i++)
+         {
+             curr_sum+=arr[i];
+         }
+
+
+        for (int i=k;i<arr.length;i++)
+        {
+          curr_sum = curr_sum+arr[i]-arr[i-k];
+
+          if (curr_sum>max_sum)
+          {
+              max_sum=curr_sum;
+          }
+
+        }
+        return max_sum;
+
+
+
+
+    }
+
+
+
 }
