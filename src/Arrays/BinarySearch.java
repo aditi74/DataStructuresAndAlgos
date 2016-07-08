425ed72322168ce9770795392f31aaa5c2b84b2f
Aditi Sharma <sharmaaditi.nitsri@gmail.com>
2016-07-08 18:57:57 +0530

    Interview Questions

--- src/Arrays/BinarySearch.java
+++ src/Arrays/BinarySearch.java
@@ -7,18 +7,37 @@ public class BinarySearch {
 
     public static void main(String[] args) {
         int[] arr ={1,2,3,4,5,6,7};
-      boolean search=  binarySearch(arr,1,0,arr.length-1);
+       int search=  binarySearchIterative(arr,1,0,arr.length-1);
         System.out.println("Search successful :"+search);
     }
 
-    public static boolean binarySearch(int[] arr,int num,int low,int high) {
+    public static int binarySearch(int[] arr,int num,int low,int high) {
         if(low <= high) {
             int mid = (low + high) / 2;
 
-            if (num == arr[mid]) return true;
+            if (num == arr[mid]) return mid;
             if (num < arr[mid]) return binarySearch(arr, num, low, mid - 1);
             if (num > arr[mid]) return binarySearch(arr, num, mid + 1, high);
 
         }
-      return false;}
+      return -1;}
+
+    public static int binarySearchIterative(int[] arr,int num,int low,int high){
+        while (low<=high)
+        {
+            int mid= (low+high)/2;
+            if (arr[mid]< num )
+                  low=mid+1;
+            if (arr[mid]>num)
+                high=mid-1;
+            if (arr[mid]==num)
+                return mid;
+        }
+        return -1;
+
+
+
+    }
+
+
 }
